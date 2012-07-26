43bff9bb83831c7baa927d3504c0ce2bcdc3c107
Paul Mederos <paul@primeenterprise.net>
2012-07-26 14:33:23 -0400

    Removed routes for User actions. No need for them at the moment.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 DistilledIntelligence::Application.routes.draw do
 
-  match 'user/edit' => 'users#edit', :as => :edit_current_user
-  match 'signup' => 'users#new', :as => :signup
-  match 'logout' => 'sessions#destroy', :as => :logout
-  match 'login' => 'sessions#new', :as => :login
+  # match 'user/edit' => 'users#edit', :as => :edit_current_user
+  # match 'signup' => 'users#new', :as => :signup
+  # match 'logout' => 'sessions#destroy', :as => :logout
+  # match 'login' => 'sessions#new', :as => :login
 
-  resources :sessions
-
-  resources :users
+  # resources :sessions
+  # resources :users
 
   root :to => "pages#home"
 end
