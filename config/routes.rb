9b012938428b12934d9fd0f47241547b021928d9
Paul Mederos <paul@primeenterprise.net>
2012-07-26 12:27:30 -0400

    Initial commit. Pulled from rails-startup template.

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+DistilledIntelligence::Application.routes.draw do
+
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+  match 'signup' => 'users#new', :as => :signup
+  match 'logout' => 'sessions#destroy', :as => :logout
+  match 'login' => 'sessions#new', :as => :login
+
+  resources :sessions
+
+  resources :users
+
+  root :to => "pages#home"
+end
