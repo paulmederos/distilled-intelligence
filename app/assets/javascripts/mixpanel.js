12db191dbcf03da3a8112e910d7feb9061a93a32
Paul Mederos <pvm@enchant.co>
2012-07-30 15:00:08 -0400

    Added Mixpanel tracking for events.

---
+++ app/assets/javascripts/mixpanel.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  mixpanel.track("Visited DI Site");
+
+ 	$("#become-a-sponsor").click(function() {
+	  mixpanel.track("Sponsor was interested");
+	});
+
+    $("#main-cta").click(function() {
+	  mixpanel.track("Header CTA was clicked");
+	});
+
+	$("#why-apply-cta").click(function() {
+	  mixpanel.track("'Why Apply?' CTA was clicked");
+	});
+
+	$("#why-apply-ticket-interest").click(function() {
+	  mixpanel.track("'Why Apply?' purchase ticket was clicked");
+	});
+
+	$(".graduate-link").click(function() {
+	  mixpanel.track("A graduate was clicked");
+	});
+
+	$("#register-now").click(function() {
+	  mixpanel.track("Final registration CTA was clicked");
+	});
+
+	$(".footer-link").click(function() {
+	  mixpanel.track("A footer link was clicked");
+	});
+});
